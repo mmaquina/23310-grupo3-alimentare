99d4a5e3bcc594c6a4d492d75d5389d48f0d4f61
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-05-05 20:07:41 -0300

    Update App.js

--- alimentare/src/App.js
+++ alimentare/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Importar Componentes
+import NavBar from './components/Navbar';
+import Inicio from './components/Inicio';
+import Agendas from './components/Agendas';
+import Calendarios from './components/Calendarios';
+import Cuadernos from './components/Cuadernos';
+import Contacto from './components/Contacto';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-    <h2>Hola</h2>
-    </div>
+    <>
+      <NavBar/>
+      
+      <Routes>
+         <Route path='/'element={ <Inicio />}/>
+         <Route path='/Agendas' element={ <Agendas/>}/>
+         <Route path='/Calendarios' element={ <Calendarios/>}/>
+         <Route path='/Cuadernos' element={ <Cuadernos/>}/>
+         <Route path='/Contacto'  element={<Contacto/>}/>
+      </Routes>
+          
+      <Footer/>
+
+    </>
   );
 }
 
