2d491923a62473c37116d51a7282a3d2238bc2ad
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-05-12 21:31:43 -0300

    20230512_Contacto

--- alimentare/src/Paginas/Contacto.jsx
+++ alimentare/src/Paginas/Contacto.jsx
@@ -3,21 +3,22 @@ import { MDBInput,MDBTextArea, MDBCheckbox, MDBBtn } from 'mdb-react-ui-kit';
 
 export default function App() {
   return (
+    
     <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
-      <h2>Contact us</h2>
+      <h2>Contacto</h2>
 
-      <MDBInput label='Name' v-model='name' wrapperClass='mb-4' />
+      <MDBInput label='Nombre' v-model='name' wrapperClass='mb-4' />
 
-      <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' />
+      <MDBInput type='email' label='Direccion Email' v-model='email' wrapperClass='mb-4' />
 
-      <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' />
+      <MDBInput label='Asunto' v-model='subject' wrapperClass='mb-4' />
 
-      <MDBTextArea wrapperClass='mb-4' label='Message' />
+      <MDBTextArea wrapperClass='mb-4' label='Consulta' />
 
-      <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
+      <MDBCheckbox wrapperClass='d-flex justify-content-center' label=' Recibir una copia' />
 
       <MDBBtn color='primary' block className='my-4'>
-        Send
+        Enviar
       </MDBBtn>
     </form>
   );
