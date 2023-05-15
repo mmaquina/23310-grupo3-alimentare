43c0a5f2b7385f094454627a772670f41639028e
CamiloLondonoDev <camilo.londono.dev@gmail.com>
2023-05-15 19:04:21 -0300

    login
    - Cree el login básico,
    -Cree la carpeta componentes y pase los componentes ahí
    - Comente para no pisar código del navbar y del Footer que estaba de antes pero no compilaba por que no tenia los archivos del logo ni los routes del navbar de sakura

--- alimentare/src/App.js
+++ alimentare/src/App.js
@@ -2,27 +2,32 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import LoginComponent from "./componentes/login.jsx";
+
+
 //Importar Componentes
-import NavBar from './components/Navbar';
-import Inicio from './components/Inicio';
-import Agendas from './components/Agendas';
-import Calendarios from './components/Calendarios';
-import Cuadernos from './components/Cuadernos';
-import Contacto from './components/Contacto';
-import Footer from './components/Footer';
+import LoginComponent from "./componentes/login";
+ import navbarPrincipal from './componentes/Navbar';
+// import Inicio from './components/Inicio';
+// import Agendas from './components/Agendas';
+// import Calendarios from './components/Calendarios';
+// import Cuadernos from './components/Cuadernos';
+// import Contacto from './components/Contacto';
+ import Footer from './componentes/Footer';
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <navbarPrincipal/>
+      <LoginComponent/>
       
-      <Routes>
+      {/* <Routes>
          <Route path='/'element={ <Inicio />}/>
          <Route path='/Agendas' element={ <Agendas/>}/>
          <Route path='/Calendarios' element={ <Calendarios/>}/>
          <Route path='/Cuadernos' element={ <Cuadernos/>}/>
          <Route path='/Contacto'  element={<Contacto/>}/>
-      </Routes>
+      </Routes> */}
           
       <Footer/>
 
---
+++ alimentare/src/componentes/Navbar.jsx
@@ -0,0 +1,27 @@
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import { Link} from 'react-router-dom';
+// import Logo from '../assets/Imagenes/Sakura.png'
+
+const navbarPrincipal = () => {
+  return (
+    <Navbar className="navBg" bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/" >
+            {/* <img src={Logo} alt="SakuraArteEnPapel" width="100" height="100" className='d-inline-block align-middle'/> Sakura Arte en Papel */}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/Agendas">Agendas</Nav.Link>
+            <Nav.Link as={Link} to="/Cuadernos">Cuadernos</Nav.Link>
+            <Nav.Link as={Link} to="/Calendarios">Calendarios</Nav.Link>
+            <Nav.Link as={Link} to="/Contacto">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default navbarPrincipal;
