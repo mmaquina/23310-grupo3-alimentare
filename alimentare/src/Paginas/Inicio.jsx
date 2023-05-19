93757b16802fe340b08e4a0f3c1da9dfc3dff243
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-05-19 19:59:44 -0300

    20230519_Inicio

--- alimentare/src/Paginas/Inicio.jsx
+++ alimentare/src/Paginas/Inicio.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import CardImagen from '../Imagenes/verduras.png'
 
-export default function Inicio() {
+function GridExample() {
   return (
-    <body>
-      <p className="col-md-4 mb-0 text-body-secondary">&copy; Alimentare </p>
-    </body>
-    
-  )
+    <Row xs={2} md={4} className="xs-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={CardImagen} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
+
+export default GridExample;
