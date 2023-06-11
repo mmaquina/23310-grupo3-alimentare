16ee256a9ea6f78bd390f9dd64689b1bf9f9cebc
mmaquina <mmaquina@gmail.com>
2023-06-11 11:23:12 -0300

    manda url de la imagen por props - trunca receta

--- alimentare/src/Componentes/recetas/GaleriaRecetas.jsx
+++ alimentare/src/Componentes/recetas/GaleriaRecetas.jsx
@@ -3,7 +3,23 @@ import { React, useState, useEffect } from "react";
 import { getItems } from '../firebase/utils.jsx';
 import db from '../firebase/FirebaseConfig.jsx';
 import { MDBRow } from 'mdb-react-ui-kit';
+
 const COLECCION_RECETAS = 'recetas';
+
+function limitString(inputString, limit) {
+  if (inputString.length <= limit) {
+    return inputString; // Return the original string if it's already within the limit
+  }
+  
+  // Find the last space before the limit
+  const lastSpaceIndex = inputString.lastIndexOf(' ', limit);
+
+  if (lastSpaceIndex === -1) {
+    return inputString.substring(0, limit); // If there are no spaces within the limit, return the substring
+  } else {
+    return inputString.substring(0, lastSpaceIndex); // Return the substring until the last space within the limit
+  }
+}
 
 export default function Recetas() {
   const [isLoading, setIsLoading] = useState(true);
@@ -46,10 +62,16 @@ export default function Recetas() {
       <h2>Recetas</h2>
       <div className="container  flex  flex-wrap text-center">
         <MDBRow className='row-cols-md-4 g-4'>
-            <Tarjeta receta={{Titulo: "Añadir receta", Receta: "Pulse para colaborar con su propia receta a la página."}}/>
+            <Tarjeta receta={{
+              Titulo: "Añadir receta", 
+              Receta: "Pulse para colaborar con su propia receta a la página.",
+              foto: 'https://icon-library.com/images/add-photo-icon/add-photo-icon-17.jpg'}}/>
         
             {recetas.map((receta, index) => {
-              
+              console.log(receta)
+              receta.foto = 'https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png';
+              receta.Receta = limitString(receta.Receta, 40) + '...';
+
               return (
                 <div key={index}>
                   <Tarjeta receta={receta} />
--- alimentare/src/Componentes/recetas/RecetaCard.jsx
+++ alimentare/src/Componentes/recetas/RecetaCard.jsx
@@ -11,9 +11,8 @@ import {
 export default function Tarjeta(props) {
   return (
     <MDBCol>
-
         <MDBCard className='h-100'>
-          <a href='/subirreceta'><MDBCardImage src='https://icon-library.com/images/add-photo-icon/add-photo-icon-17.jpg' position='top' alt='Agregar receta' /></a>
+          <a href='/subirreceta'><MDBCardImage src={props.receta.foto} position='top' alt='Agregar receta' height={100}/></a>
           <MDBCardBody>
             <MDBCardTitle>{props.receta.Titulo}</MDBCardTitle>
             <MDBCardText>
