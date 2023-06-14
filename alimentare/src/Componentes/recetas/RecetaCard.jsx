d8dee997f8e0269edc8aa64593c707d56b7f936c
mmaquina <mmaquina@gmail.com>
2023-06-13 22:52:12 -0300

    Cards bootstrap (#21)
    * tarjetas usan bootstrap

--- alimentare/src/Componentes/recetas/RecetaCard.jsx
+++ alimentare/src/Componentes/recetas/RecetaCard.jsx
@@ -1,34 +1,26 @@
-import React from 'react';
-import {
-  MDBCard,
-  MDBCardImage,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCol
-} from 'mdb-react-ui-kit';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 export default function Tarjeta(props) {
   let link = '#';
   if (props.receta.subirReceta) {
     link = '/subirreceta';
   } 
-
   return (
-    <MDBCol style={{ height: '250px'}}>
-        <MDBCard className='h-100' >
-          <a href={link}><MDBCardImage 
+        <Col>
+          <Card>
+          <a href={link}><Card.Img
             src={props.receta.foto} 
             position='top' 
             alt='Agregar receta' 
-            style={{ maxHeight: '150px', width: '100%', objectFit: 'contain'}} /></a>
-          <MDBCardBody>
-            <MDBCardTitle>{props.receta.Titulo}</MDBCardTitle>
-            <MDBCardText>
+            style={{ maxHeight: '150px', width: '100%', objectFit: 'contain'}} 
+          /></a>
+          <Card.Body>
+            <Card.Title>{props.receta.Titulo}</Card.Title>
+            <Card.Text>
               {props.receta.Receta}
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-    </MDBCol>
-  );
-}
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+      )}
