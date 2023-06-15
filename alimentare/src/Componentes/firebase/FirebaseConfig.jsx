277c6fc0836f8d22d4933887bf4887065ee50f84
CamiloLondonoDev <129802277+CamiloLondonoDev@users.noreply.github.com>
2023-06-15 00:56:08 -0300

    Login de usuarios (#22)
    * Sign In y sign Up funcional
    
    -Falta mensaje de  success cuando efectivamente crea el usuario y cuando inicia sesión.
    -Falta borrado de los inputs cuando se envía el formulario
    
    * logout y iniciar sesion con google
    
    * boton logout en navbar
    
    * Update alimentare/src/Componentes/firebase/FirebaseConfig.jsx
    
    ---------
    
    Co-authored-by: mmaquina <mmaquina@gmail.com>

--- alimentare/src/Componentes/firebase/FirebaseConfig.jsx
+++ alimentare/src/Componentes/firebase/FirebaseConfig.jsx
@@ -1,24 +1,33 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.APIKEY,
+//   authDomain: "timetracker-374421.firebaseapp.com",
+//   projectId: "timetracker-374421",
+//   storageBucket: "timetracker-374421.appspot.com",
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APPID,
+//   measurementId: process.env.MEASUREMENTID
+// };
+
+//Configuración de base de datos para rama de Camilo
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: "timetracker-374421.firebaseapp.com",
-  projectId: "timetracker-374421",
-  storageBucket: "timetracker-374421.appspot.com",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 
 
 export default db;
