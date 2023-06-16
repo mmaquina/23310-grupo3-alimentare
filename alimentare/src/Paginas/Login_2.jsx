d29b1a53d7be8dfbd364cfca2d1f5f348763fc8b
CamiloLondonoDev <129802277+CamiloLondonoDev@users.noreply.github.com>
2023-06-16 00:00:03 -0300

    muestra mensaje devuelto por la base de datos
    muestra mensaje devuelto por la base de datos como mensaje de feedback al usuario cuando hace login con mail/clave incorrecta
    -TODO: mensaje correcto de login exitoso

--- alimentare/src/Paginas/Login_2.jsx
+++ alimentare/src/Paginas/Login_2.jsx
@@ -1,120 +1,101 @@
 import { auth, googleProvider } from "../Componentes/firebase/FirebaseConfig";
-import { signInWithEmailAndPassword, getAuth, signInWithPopup, } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth, signInWithPopup } from "firebase/auth";
 import React, { useState } from "react";
-//import ReactDOM from "react-dom";
 import '../Style/Login_2.css';
+import { Button } from "react-bootstrap";
 
 function App() {
-  // React States
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-  console.log(auth?.currentUser?.email)//Para saber quien esta logueado. ademas los signos de interrogacion sirven para indicar que no lea una variable antes de que tenga valor por eso no da error si no hay nadie logueado
-  
+  let usuario = auth?.currentUser?.email;
+  console.log("Usuario: " + usuario);
 
   const signIn = async () => {
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
         const user = userCredential.user;
         console.log(user);
-        // ...
+        setMessage("Bienvenido: " + auth.email ); //TODO muestra el usuario logeado como undefined
       })
       .catch((error) => {
         const errorCode = error.code;
+        console.log(errorCode)
         const errorMessage = error.message;
+        switch (errorCode) {
+          case "auth/wrong-password":
+            setMessage("La contraseña es incorrecta");
+            break;
+          case "auth/user-not-found":
+            setMessage("El usuario no ha sido encontrado");
+            break;
+          case "auth/invalid-email":
+            setMessage("El correo electrónico es inválido");
+            break;
+          case "auth/missing-password":
+            setMessage("La contraseña no ha sido proporcionada");
+            break;
+          default:
+            setMessage("");
+            break;
+        }        
       });
-  }
+  };
 
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider)
-
+      await signInWithPopup(auth, googleProvider);
+      setMessage("Bienvenido: " + auth.email);
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      setMessage("Error al iniciar sesión con Google");
     }
   };
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid email",
-    pass: "invalid password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
   const renderForm = (
     <div className="form">
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="input-container">
           <label>Correo electronico </label>
-          <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} name="uname" required />
-          {renderErrorMessage("uname")}
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            name="uname"
+            required
+          />
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} name="pass" required />
-          {renderErrorMessage("pass")}
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            name="pass"
+            required
+          />
         </div>
         <div className="button-container">
-          <input type="submit" onClick={signIn} />
+          <Button variant="success" onClick={signIn}>Enviar</Button>
         </div>
+        <p>{message}</p>
         <br></br>
         <p>
-          <button onClick={signInWithGoogle} >Iniciar sesión con Google</button>
+          <button onClick={signInWithGoogle}>Iniciar sesión con Google</button>
         </p>
 
         <p>
-          <a class="text-muted link-info" href="#!" >¿Olvidaste tu password?</a>
+          <a className="text-muted link-info" href="#!">
+            ¿Olvidaste tu password?
+          </a>
         </p>
-        <p>¿Todavía no tenés una cuenta? 
-          <a href='/Registro' class="link-info">Registrate</a>
-        </p>
-
-        <p>Usuario logueado 
-          <p ></p>
+        <p>
+          ¿Todavía no tenés una cuenta?
+          <a href="/Registro" className="link-info">
+            Registrate
+          </a>
         </p>
       </form>
     </div>
@@ -124,7 +105,7 @@ function App() {
     <div className="app">
       <div className="login-form">
         <div className="title">Login</div>
-        {isSubmitted ? <div>El usuario ha iniciado sesión con éxito</div> : renderForm}
+        {renderForm}
       </div>
     </div>
   );
