7255448ba5d75ddda1acb4b2be0b1c61aac5e3f1
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-06-16 20:35:24 -0300

    20230616

--- alimentare/src/Paginas/Inicio_2.jsx
+++ alimentare/src/Paginas/Inicio_2.jsx
@@ -1,59 +1,39 @@
-//import "../Style/Inicio_2.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "../Style/Inicio_2.css";
 // import Card from 'react-bootstrap/Card';
 // import Col from 'react-bootstrap/Col';
 // import Row from 'react-bootstrap/Row';
 // import CardImagen from '../Imagenes/verduras.png'
 // import { Container} from 'react-bootstrap';
 
-
-function card() {
-  return (
-    
-<div className="container_i">
-        <div className="card" style="--clr: #009688">
-            <div className="img-box">
-                <img src="https://i.postimg.cc/t4w95jsf/img-01.png"/>
-            </div>
-            <div className="content">
-                <h2>Leafs</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Architecto, hic? Magnam eum error saepe doloribus corrupti
-                    repellat quisquam alias doloremque!
-                </p>
-                <a href="">Read More</a>
-            </div>
-        </div>
-        <div className="card" style="--clr: #FF3E7F">
-            <div className="img-box">
-                <img src="https://i.postimg.cc/pdjRc68d/img-02.png"/>
-            </div>
-            <div className="content">
-                <h2>Fruits</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Architecto, hic? Magnam eum error saepe doloribus corrupti
-                    repellat quisquam alias doloremque!
-                </p>
-                <a href="">Read More</a>
-            </div>
-        </div>
-        <div className="card" style="--clr: #03A9F4">
-            <div className="img-box">
-                <img src="https://i.postimg.cc/26fms7F7/img-03.png"/>
-            </div>
-            <div className="content">
-                <h2>Flowers</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Architecto, hic? Magnam eum error saepe doloribus corrupti
-                    repellat quisquam alias doloremque!
-                </p>
-                <a href="">Read More</a>
-            </div>
-        </div>
-    </div>
-  );
-}
-
-export default card;
+class ReactCard extends React.Component{
+    constructor(props) {
+       super(props);
+       this.state = {
+                    date: new Date(),
+                    name: "Zae",
+                    rel: "friends"};
+     }
+   changeName = (e) =>{
+          this.setState({name: e.target.value});
+        }
+     render(){
+       return(
+        <div className="card">
+         <div className="card-body">
+           <h5 className="card-title">Hello {this.state.name}!</h5>
+           <p className="card-text">Let´s be {this.state.rel} since today {this.state.date.toLocaleTimeString()}.</p>      
+           <form>
+             <div className="form-group">
+               <label>Tell me your name </label>
+               <input type="text" className="form-control" id="exampleInputText"  onChange = {this.changeName} />
+             </div>
+           </form>
+         </div>
+       </div>
+       )
+     }  
+   }
+   
+   ReactDOM.render(<ReactCard />, document.getElementById('reactCard'));
