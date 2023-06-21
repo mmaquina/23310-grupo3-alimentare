4c9fb226f99e05ff0b8db0f2d13ea0bc2b3863c2
mmaquina <mmaquina@gmail.com>
2023-06-21 02:19:46 -0300

    arregla bug en galeria de alimentos

--- alimentare/src/Componentes/alimento/GaleriaAlimentos.jsx
+++ alimentare/src/Componentes/alimento/GaleriaAlimentos.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import { Container} from 'react-bootstrap';
 
 const ENDPOINT = "https://trackapi.nutritionix.com/v2/search/instant?query="
-const prods = ["almonds", "honey", "nuts"]
+const prods = ["almonds", "honey", "nuts", "cheese", "flour", "quinoa", "olive oil"]
 
 export default function Alimentos() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,13 +19,19 @@ export default function Alimentos() {
         "x-app-key": process.env.REACT_APP_NUTRITIONIX_KEY
       }})
         .then((response) => response.json())
-          .then((prodj)=> prodlist.push(prodj.common[0]))
+          .then((prodj)=> {
+            prodlist.push(prodj.common[0]);
+            if (prodlist.length === prods.length) {
+              setAlimentos(prodlist);
+              setIsLoading(false);
+            }
+          });
       });
-    setAlimentos(prodlist);
-    setIsLoading(false);
+
+    console.log("prodlist",prodlist)
   }, []);
 
-  if (isLoading ) { // ⬅️ si está cargando, mostramos un texto que lo indique
+  if (isLoading || productos === null) { // ⬅️ si está cargando, mostramos un texto que lo indique
     return (
       <div className="App">
         <h1>Cargando...</h1>
@@ -38,16 +44,20 @@ export default function Alimentos() {
       <h2>Alimentos</h2>
       <div className="container flex flex-wrap text-center">
         <Container className='me-auto' >
+        container
 
           <Row md={5} className="g-5">
+            row
             {productos.map((producto, index) => {
               producto.foto = producto.photo.thumb
+              console.log(producto)
               return (
                 <div key={index}>
                   <Tarjeta producto={producto} />
                 </div>
               );
             })}
+            row cierre
           </Row>
 
         </Container>
