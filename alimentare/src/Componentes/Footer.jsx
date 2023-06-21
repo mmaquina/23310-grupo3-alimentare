b1a2b729d068437ed063c1d86d454ecb5075245d
VivianaCabeza <127574746+VivianaCabeza@users.noreply.github.com>
2023-06-21 20:17:08 -0300

    Intro recetas vivi (#28)
    * Se modifica inicio para dar formato a la pagina
    
    * modifique el login
    
    * eliminado archivo alimentos

--- alimentare/src/Componentes/Footer.jsx
+++ alimentare/src/Componentes/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
    
 <div className="container">
-  <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+  <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
     <p className="col-md-4 mb-0 text-body-secondary">&copy;Alimentare </p>
 
     <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
